d3974e5e995cbb50b5c935b33db3ba3514b3ca51
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-10-12 18:53:04 -0600

    Create new instances of card with start game method

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,20 @@
+const Card = require('./Card');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
-
+  constructor(round) {
+    this.currentRound = round;
+    this.card;
+  }
+  start() {
+    const allTheCards = prototypeQuestions.map(element => {
+      this.card = new Card(element.id, element.question, element.answers, element.correctAnswer)
+      return
+    });
+    this.currentRound.deck.cards.push(allTheCards)
+  }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
@@ -16,3 +26,8 @@ class Game {
 }
 
 module.exports = Game;
+
+
+//pseudocode: properties
+  //currentRound property
+//pseudocode: methods
--- test/Game-test.js
+++ test/Game-test.js
@@ -3,17 +3,37 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Turns = require('../src/Turns');
+const Game = require('../src/Game');
+const { prototype } = require('mocha');
 const expect = chai.expect;
 
 
 describe('Game', function(){
-    let card1, card2, card3, deck, round;
+    let cards, deck, round, game;
     beforeEach(function() {
-        card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-        card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
-        card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
-        deck = new Deck([card1, card2, card3])
+        // card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        // card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+        // card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
+        // deck = new Deck([card1, card2, card3])
+        // cards = {prototype}
+        deck = new Deck([])
         round = new Round(deck);
+        game = new Game(round);
     });
-    
+
+    it('should be a function', function() {
+        expect(Game).to.be.a('function');
+    });
+
+    it('should keep track of round', function() {
+        expect(game.currentRound).to.deep.equal(round)
+    });
+
+    it('should have a property of card', function() {
+        expect(game.card).to.equal();
+    })
+
+    it('should have a method to start game', function() {
+        expect(game.start()).to.equal()
+    })
 })
